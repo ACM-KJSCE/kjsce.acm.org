7edefeb24eea59e4a6f741b0357abd44d64394b3
robaro12345 <omkarprajakta@gmail.com>
2025-12-31 19:21:10 +0530

    Ha

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,31 +1,30 @@
 import React, { Suspense, useRef } from "react";
 import { WavyBackground } from "./ui/wavy-background";
-import "./styles/Hero.css"
-import { World } from "./ui/globe";
 import sampleArcs from "../data/SampleArcs";
 import globeConfig from "../data/GlobeConfig";
+import "./styles/Hero.css";
 
-// const World = React.lazy(() => import("./ui/globe"));
+const World = React.lazy(() =>
+  import("./ui/globe").then((module) => ({ default: module.World })),
+);
 
 function Hero() {
-
   const modelRef = useRef();
   return (
     <WavyBackground className="max-w-7xl mx-auto px-4 py-16 sm:mt-64 md:mt-96 mt-72 lg:mt-0">
-
       <div className="grid grid-cols-1 lg:grid-cols-2 lg:gap-20 gap-0 items-center">
         {/* Left Side: Text Content */}
         <div className="flex absolute top-0 left-0 w-full">
-        {/* <div className="glow"></div> */}
+          {/* <div className="glow"></div> */}
         </div>
         <div className="text-white text-center lg:text-left">
           <div className="flex justify-center items-center">
             <img className="w-52 md:w-44" src="logo_withoutbg.png" alt="" />
           </div>
           <div className="text-4xl md:text-5xl lg:text-6xl font-bold mb:mb-6 md:mt-2 mt-8 opacity-90">
-            We are KJSCE - <span className="text-blue-300 opacity-100">ACM</span>
+            We are KJSCE -{" "}
+            <span className="text-blue-300 opacity-100">ACM</span>
           </div>
-
 
           <div className="mt-8">
             <p className="text-xl md:text-2xl font-medium">
@@ -36,7 +35,8 @@ function Hero() {
         </div>
 
         {/* Right Side: Globe */}
-        <div className="relative lg:h-[36rem] md:h-[30rem] h-[20rem] w-full flex justify-center items-center">
+        <div className="relative lg:h-[35rem] md:h-[30rem] h-[20rem] w-full flex justify-center items-center">
+          <Suspense fallback={<div className="text-white">Loading...</div>}>
             <World data={sampleArcs} globeConfig={globeConfig} />
             {/* <model-viewer src="/models/Earth.glb"
                   ios-src="/models/Earth.usdz"
@@ -53,10 +53,7 @@ function Hero() {
                   ref={(ref) => {
                     modelRef.current = ref;
                   }}/> */}
-            
-          {/* </Suspense> */}
-          
-
+          </Suspense>
         </div>
       </div>
     </WavyBackground>
