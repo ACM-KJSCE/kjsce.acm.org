740079a0dca3f212ef65dc01fc6a365683928044
Varun Mutha <muthavarun07@gmail.com>
2026-09-10 16:13:58 +0530

    Feat: Added Sponsors section for the btc-2026

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,13 @@ import TeamShowcase from "./components/TeamShowcase";
 import Clarity from "@microsoft/clarity";
 import "./App.css";
 import Sponsors from "./components/Sponsors";
+import sponsors from "./data/sponsors.json";
 
 function App() {
   const projectId = "pjgnnov8ie";
   Clarity.init(projectId);
+  const sponsorList = sponsors.sponsors.map((sponsor) => sponsor.url);
+
   return (
     <>
       <Navbar />
@@ -21,7 +24,7 @@ function App() {
           <Hero />
           <AboutUs />
           <Event />
-          <Sponsors />
+          <Sponsors sponsorList={sponsorList} />
           <Team2 />
           <TeamShowcase />
         </div>
--- src/components/Sponsors.jsx
+++ src/components/Sponsors.jsx
@@ -29,9 +29,12 @@ function Sponsors({ sponsorList }) {
   return sponsorList ? (
     <div
       id="sponsors"
-      className=" font-semibold text-1xl md:text-2xl text-white text-justify sm:pl-4"
+      className="flex flex-col items-center font-semibold text-1xl md:text-2xl text-white text-justify sm:pl-4 m-"
     >
-      <h1 className="mb-4">Event Sponsors</h1>
+    <h1 className="text-4xl uppercase font-black text-center dark:text-white text-black mb-12 border-b-cyan-400 border-b-2 inline-block mx-auto pb-4">
+          Our Sponsors
+        </h1>
+
       <div className="overflow-hidden  w-full h-auto ml-1">
         <div
           className="relative flex w-full"
@@ -39,16 +42,16 @@ function Sponsors({ sponsorList }) {
           onMouseLeave={() => setIsHovered(false)}
         >
           <motion.div
-            className="flex gap-4 overflow-hidden w-full"
+            className="flex gap-8 overflow-hidden w-full"
             ref={sliderRef}
           >
-            {[...sponsorList, ...sponsorList, ...sponsorList].map(
+            {[...sponsorList, ...sponsorList, ...sponsorList, ...sponsorList, ...sponsorList].map(
               (imgSrc, index) => (
                 <div key={index} className="flex-shrink-0">
                   <img
                     src={imgSrc}
                     alt={`Slide ${index}`}
-                    className="w-auto h-[60px] object-cover"
+                    className="w-auto h-[90px] object-cover"
                   />
                 </div>
               ),
